d14478901d7edffd160fe31b0e3402f70cb28b31
Mhdi <161378550+Mhdiqpzx@users.noreply.github.com>
2024-05-22 10:32:31 +0330

    Update mahdi-wow.sh

--- mahdi-wow.sh
+++ mahdi-wow.sh
@@ -245,8 +245,8 @@ endipresult() {
 		"tag": "select",
 		"outbounds": [
 			"auto",
-			"IP->Iran🇮🇷, Telegram: mhdi_h5",
-			"IP->Germany🇩🇪, Telegram: mhdi_h5"
+			"IP Iran->t.me/mhdi_h5",
+			"IP Main->t.me/mhdi_h5"
 		],
 		"default": "auto"
 		},
@@ -254,15 +254,15 @@ endipresult() {
 		"type": "urltest",
 		"tag": "auto",
 		"outbounds": [
-			"IP->Iran🇮🇷, Telegram: mhdi_h5",
-			"IP->Germany🇩🇪, Telegram: mhdi_h5"
+			"IP Iran->t.me/mhdi_h5",
+			"IP Main->t.me/mhdi_h5"
 		],
 		"url": "http://cp.cloudflare.com/",
 		"interval": "10m0s"
 		},
 		{
 		"type": "wireguard",
-		"tag": "IP->Iran🇮🇷, Telegram: mhdi_h5",
+		"tag": "IP Iran->t.me/mhdi_h5",
 		"local_address": [
 			"172.16.0.2/32",
 			"'$publicKey'"
@@ -277,8 +277,8 @@ endipresult() {
 		},
 		{
 		"type": "wireguard",
-		"tag": "IP->Germany🇩🇪, Telegram: mhdi_h5",
-		"detour": "IP->Iran🇮🇷, Telegram: mhdi_h5",
+		"tag": "IP Main->t.me/mhdi_h5",
+		"detour": "IP Iran->t.me/mhdi_h5",
 		"local_address": [
 			"172.16.0.2/32",
 			"'$publicKey2'"
